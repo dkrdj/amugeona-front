cf9bd1cb7cc0db67fcb0e52241db96b4ed9d7a27
dkrdj <dkrdj1000@naver.com>
2022-12-15 03:25:44 +0900

    changepasswordLayout fix

--- lib/ChangePasswordLayout.dart
+++ lib/ChangePasswordLayout.dart
@@ -17,155 +17,169 @@ class _ChangePasswordLayout extends State<ChangePasswordLayout> {
       final double width = constraints.maxWidth;
       final double height = constraints.maxHeight;
 
-      return Center(
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding:
-                      EdgeInsets.fromLTRB(width / 8, height / 15, width / 8, 0),
-                  child: Text(
-                    '비밀번호 변경',
-                    style: TextStyle(
-                      fontSize: width / 18,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: width / 200,
-                    ),
+      return Column(
+        children: <Widget>[
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: width / 14, top: height / 20),
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  size: width / 18,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: height / 18),
+                child: Text(
+                  'Back',
+                  style: TextStyle(
+                    fontSize: width / 25,
                   ),
                 ),
-              ],
+              ),
+            ],
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.fromLTRB(width / 8, height / 40, width / 8, 0),
+            child: Text(
+              '비밀번호 변경',
+              style: TextStyle(
+                fontSize: width / 15,
+                fontWeight: FontWeight.w600,
+                letterSpacing: width / 200,
+              ),
             ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(
-                      width / 8, height / 20, width / 8, height / 100),
-                  child: Text(
-                    '현재 비밀번호',
-                    style: TextStyle(
-                      fontSize: width / 36,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.pinkAccent,
-                      letterSpacing: width / 500,
-                    ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    width / 8, height / 20, width / 8, height / 100),
+                child: Text(
+                  '현재 비밀번호',
+                  style: TextStyle(
+                    fontSize: width / 36,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.pinkAccent,
+                    letterSpacing: width / 500,
                   ),
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(width / 8, 0, width / 8, 0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  isDense: true,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black26,
-                    ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(width / 8, 0, width / 8, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                isDense: true,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.black26,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black,
-                    ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.black,
                   ),
                 ),
               ),
             ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(
-                      width / 8, height / 20, width / 8, height / 100),
-                  child: Text(
-                    '새 비밀번호',
-                    style: TextStyle(
-                      fontSize: width / 36,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.pinkAccent,
-                      letterSpacing: width / 500,
-                    ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    width / 8, height / 20, width / 8, height / 100),
+                child: Text(
+                  '새 비밀번호',
+                  style: TextStyle(
+                    fontSize: width / 36,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.pinkAccent,
+                    letterSpacing: width / 500,
                   ),
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(width / 8, 0, width / 8, 0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  isDense: true,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black26,
-                    ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(width / 8, 0, width / 8, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                isDense: true,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.black26,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black,
-                    ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.black,
                   ),
                 ),
               ),
             ),
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(
-                      width / 8, height / 20, width / 8, height / 100),
-                  child: Text(
-                    '새 비밀번호 확인',
-                    style: TextStyle(
-                      fontSize: width / 36,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.pinkAccent,
-                      letterSpacing: width / 500,
-                    ),
+          ),
+          Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(
+                    width / 8, height / 20, width / 8, height / 100),
+                child: Text(
+                  '새 비밀번호 확인',
+                  style: TextStyle(
+                    fontSize: width / 36,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.pinkAccent,
+                    letterSpacing: width / 500,
                   ),
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(width / 8, 0, width / 8, 0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  isDense: true,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black26,
-                    ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(width / 8, 0, width / 8, 0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                isDense: true,
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.black26,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2,
-                      color: Colors.black,
-                    ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 2,
+                    color: Colors.black,
                   ),
                 ),
               ),
             ),
-            SizedBox(
-              width: width - width / 4,
-              height: height / 10,
-              child: Padding(
-                padding: EdgeInsets.only(top: height / 30),
-                child: ElevatedButton(
-                  onPressed: login,
-                  child: const Text(
-                    '확인',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
+          ),
+          SizedBox(
+            width: width - width / 4,
+            height: height / 10,
+            child: Padding(
+              padding: EdgeInsets.only(top: height / 30),
+              child: ElevatedButton(
+                onPressed: login,
+                child: const Text(
+                  '확인',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       );
     });
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:amugeona/BottomNav.dart';
-import 'package:amugeona/MainLayout.dart';
+import 'package:amugeona/ChangePasswordLayout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: MainLayout(),
+        body: ChangePasswordLayout(),
       ),
     );
   }
